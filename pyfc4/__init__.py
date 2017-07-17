3e3bdb4749d8d0272dfcc4d8a4315e1a503ce088
Graham Hukill <ghukill@gmail.com>
2017-07-16 22:39:50 -0400

    scaffolding

---
+++ pyfc4/__init__.py
@@ -0,0 +1,5 @@
+# pyfc4
+
+
+__version_info__ = ('0', '1')
+__version__ = '.'.join(__version_info__)
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pyfc4',
+      version='0.1',
+      description='Python 3.x binding for Fedora Commons 4.x',
+      url='http://github.com/ghukill/pyfc4',
+      author='Graham Hukill',
+      author_email='ghukill@gmail.com',
+      license='Apache License, Version 2.0',
+      packages=['pyfc4'],
+      zip_safe=False)
