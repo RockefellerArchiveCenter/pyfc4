9db82c1b57c321a056fa2fd4cdb103097d022344
Graham Hukill <ghukill@gmail.com>
2017-07-19 15:31:25 -0400

    sketching models, adding scenarios

---
+++ pyfc4/models.py
@@ -0,0 +1,96 @@
+# pyfc4
+
+
+
+
+
+
+
+
+class Repository(object):
+	'''
+	'''
+	pass
+
+
+# Resource
+class Resource(object):
+
+	'''
+	Linked Data Platform Resource (LDPR)
+	A HTTP resource whose state is represented in any way that conforms to the simple lifecycle patterns and conventions in section 4. Linked Data Platform Resources.
+	https://www.w3.org/TR/ldp/
+	'''
+	pass
+
+
+# RDF Source
+class RDFResource(Resource):
+
+	'''
+	Linked Data Platform RDF Source (LDP-RS)
+	An LDPR whose state is fully represented in RDF, corresponding to an RDF graph. See also the term RDF Source from [rdf11-concepts].
+	https://www.w3.org/TR/ldp/
+	'''
+	pass
+
+
+# Container
+class Container(RDFResource):
+	
+	'''
+	Linked Data Platform Container (LDPC)
+	A LDP-RS representing a collection of linked documents (RDF Document [rdf11-concepts] or information resources [WEBARCH]) that responds to client requests for creation, modification, and/or enumeration of its linked members and documents, and that conforms to the simple lifecycle patterns and conventions in section 5. Linked Data Platform Containers.
+	https://www.w3.org/TR/ldp/
+	'''
+	pass
+
+
+# Basic Container
+class BasicContainer(Container):
+	
+	'''
+	Linked Data Platform Basic Container (LDP-BC)
+	An LDPC that defines a simple link to its contained documents (information resources) [WEBARCH].
+	https://www.w3.org/TR/ldp/
+
+	https://gist.github.com/hectorcorrea/dc20d743583488168703
+		- "The important thing to notice is that by posting to a Basic Container, the LDP server automatically adds a triple with ldp:contains predicate pointing to the new resource created."
+	'''
+	pass
+
+
+# Direct Container
+class DirectContainer(Container):
+	
+	'''
+	Linked Data Platform Direct Container (LDP-DC)
+	An LDPC that adds the concept of membership, allowing the flexibility of choosing what form its membership triples take, and allows members to be any resources [WEBARCH], not only documents.
+	https://www.w3.org/TR/ldp/
+
+	When adding children, can also write relationships to another resource
+
+	'''
+	pass
+
+
+# Indirect Container
+class IndirectContainer(Container):
+	
+	'''
+	Linked Data Platform Indirect Container (LDP-IC)
+	An LDPC similar to a LDP-DC that is also capable of having members whose URIs are based on the content of its contained documents rather than the URIs assigned to those documents.
+	https://www.w3.org/TR/ldp/
+	'''
+	pass
+
+
+# NonRDF Source
+class NonRDFResource(Resource):
+
+	'''
+	Linked Data Platform Non-RDF Source (LDP-NR)
+	An LDPR whose state is not represented in RDF. For example, these can be binary or text documents that do not have useful RDF representations.
+	https://www.w3.org/TR/ldp/
+	'''
+	pass
---
+++ scenarios.md
@@ -0,0 +1,10 @@
+# pyfc4 scenarios
+
+## Connecting to a FC4 repository
+
+Connect to repo, get some rudimentary information.
+
+## Open resource from repo
+
+Get a pythonic handle for a resource in repository.  Probably invoke 
+
