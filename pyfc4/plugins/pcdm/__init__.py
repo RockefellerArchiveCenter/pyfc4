9f97576d190d417beb53de971b0048778e6e35ec
Graham Hukill <ghukill@gmail.com>
2017-08-16 10:15:40 -0400

    adding ore:Proxy

--- pyfc4/plugins/pcdm/__init__.py
+++ pyfc4/plugins/pcdm/__init__.py
@@ -22,58 +22,63 @@ def create_pcdm_demo_resources(repo):
 
 	# create container for all demo resources
 	pcdmdemo = _models.BasicContainer(repo,'pcdmdemo')
-	pcdmdemo.create(specify_uri=True)
+	pcdmdemo.create(specify_uri=True, auto_refresh=False)
 
 	# create generic collections
 	collections = _models.BasicContainer(repo,'pcdmdemo/collections')
-	collections.create(specify_uri=True)
+	collections.create(specify_uri=True, auto_refresh=False)
 
 	# create generic collections
 	objects = _models.BasicContainer(repo,'pcdmdemo/objects')
-	objects.create(specify_uri=True)
+	objects.create(specify_uri=True, auto_refresh=False)
 
 	# create PCDM collection "poe"
 	poe = models.PCDMCollection(repo,'pcdmdemo/collections/poe')
-	poe.create(specify_uri=True)
+	poe.create(specify_uri=True, auto_refresh=False)
 
 	# create PCDM object "raven"
 	raven = models.PCDMObject(repo,'pcdmdemo/objects/raven')
-	raven.create(specify_uri=True)
+	raven.create(specify_uri=True, auto_refresh=False)
 
 	# create proxy object at /collections/poe/members that points to /objects/raven
 	ravenproxy = models.PCDMObject(repo, 'pcdmdemo/collections/poe/members/ravenproxy')
+	ravenproxy.add_triple(ravenproxy.rdf.prefixes.rdf.type, ravenproxy.rdf.prefixes.ore.Proxy)
 	ravenproxy.add_triple(ravenproxy.rdf.prefixes.ore.proxyFor, raven.uri)
-	ravenproxy.create(specify_uri=True)
+	ravenproxy.create(specify_uri=True, auto_refresh=False)
 
 	# create PCDM object for page 1
 	ravenpage1 = models.PCDMObject(repo,'pcdmdemo/objects/ravenpage1')
-	ravenpage1.create(specify_uri=True)
+	ravenpage1.create(specify_uri=True, auto_refresh=False)
 
 	# create page text for page 1
 	ravenpage1text = _models.Binary(repo, 'pcdmdemo/objects/ravenpage1/files/text')
 	ravenpage1text.binary.data = 'Once upon a midnight dreary, while I pondered, weak and weary,'
 	ravenpage1text.binary.mimetype = 'text/plain'
-	ravenpage1text.create(specify_uri=True)
+	ravenpage1text.create(specify_uri=True, auto_refresh=False)
 
 	# create proxy object at /raven/members that points to /objects/ravenpage1
 	ravenpage1proxy = models.PCDMObject(repo, 'pcdmdemo/objects/raven/members/ravenpage1proxy')
+	ravenpage1proxy.add_triple(ravenpage1proxy.rdf.prefixes.rdf.type, ravenpage1proxy.rdf.prefixes.ore.Proxy)
 	ravenpage1proxy.add_triple(ravenpage1proxy.rdf.prefixes.ore.proxyFor, ravenpage1.uri)
-	ravenpage1proxy.create(specify_uri=True)
+	ravenpage1proxy.add_triple(ravenpage1proxy.rdf.prefixes.ore.proxyIn, raven.uri)
+	ravenpage1proxy.create(specify_uri=True, auto_refresh=False)
 
 	# create PCDM object for page 2
 	ravenpage2 = models.PCDMObject(repo,'pcdmdemo/objects/ravenpage2')
-	ravenpage2.create(specify_uri=True)
+	ravenpage2.create(specify_uri=True, auto_refresh=False)
 
 	# create page text for page 2
 	ravenpage2text = _models.Binary(repo, 'pcdmdemo/objects/ravenpage2/files/text')
 	ravenpage2text.binary.data = 'Over many a quaint and curious volume of forgotten lore'
 	ravenpage2text.binary.mimetype = 'text/plain'
-	ravenpage2text.create(specify_uri=True)
+	ravenpage2text.create(specify_uri=True, auto_refresh=False)
 
 	# create proxy object at /raven/members that points to /objects/ravenpage1
 	ravenpage2proxy = models.PCDMObject(repo, 'pcdmdemo/objects/raven/members/ravenpage2proxy')
+	ravenpage2proxy.add_triple(ravenpage2proxy.rdf.prefixes.rdf.type, ravenpage2proxy.rdf.prefixes.ore.Proxy)
 	ravenpage2proxy.add_triple(ravenpage2proxy.rdf.prefixes.ore.proxyFor, ravenpage2.uri)
-	ravenpage2proxy.create(specify_uri=True)
+	ravenpage2proxy.add_triple(ravenpage2proxy.rdf.prefixes.ore.proxyIn, raven.uri)
+	ravenpage2proxy.create(specify_uri=True, auto_refresh=False)
 
 
 
