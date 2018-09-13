b9684573143a72edc3d362c010786bcc8a89ffa0
Hillel Arnold <helrond@hotmail.com>
2018-09-13 13:10:43 -0400

    version bump and another module

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
 from setuptools import setup
 
 setup(name='pyfc4',
-      version='0.1',
+      version='0.2',
       description='Python 3 client for Fedora Commons 4',
       url='http://github.com/ghukill/pyfc4',
       author='Graham Hukill',
       author_email='ghukill@gmail.com',
       license='MIT License',
       install_requires=[
-      	'pytest',
-            'pytest-cov',
-      	'rdflib',
-      	'rdflib-jsonld',
-      	'requests'
+        'pytest',
+        'pytest-cov',
+        'rdflib',
+        'rdflib-jsonld',
+        'requests'
       ],
-      packages=['pyfc4', 'pyfc4.plugins'],
+      packages=['pyfc4', 'pyfc4.plugins', 'pyfc4.plugins.pcdm'],
       zip_safe=False)
