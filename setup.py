81c376241ed4a54dfa47f225a3b438cdb49e54ef
Graham Hukill <ghukill@gmail.com>
2018-11-07 07:49:16 -0500

    aligning tests for pcdm branch

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyfc4',
-      version='0.1',
+      version='0.3',
       description='Python 3 client for Fedora Commons 4',
       url='http://github.com/ghukill/pyfc4',
       author='Graham Hukill',
