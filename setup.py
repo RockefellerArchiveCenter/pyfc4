11652f832f29f20f8b01ffa8aad061209c70b841
Graham Hukill <ghukill@gmail.com>
2018-11-08 07:35:05 -0500

    bumping version to v0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pyfc4',
-      version='0.4',
+      version='0.5',
       description='Python 3 client for Fedora Commons 4',
       url='http://github.com/ghukill/pyfc4',
       author='Graham Hukill',
